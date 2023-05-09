ee6138477f1d50a005fdf692bef06a4bdd455bb4
gutemberg198 <patrickgutemberg1525@gmail.com>
2023-05-09 19:58:54 -0300

    atualizado

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-var tela = document.querySelector("canvas");
-var pincel = tela.getContext('2d');
 
-    pincel.fillStyle = 'lightgrey';
-    pincel.fillRect(0, 0, 800, 600);
+COMANDOS 
+//window.document.write("hello word")
+//var nome= window.prompt("qual seu nome")
+//window.alert("meu primeiro progama em java " + nome);
+//window.confirm("deseja continuar");
+//Number.parseFloat numeros quebrados 
+//Number.parseInt numeros inteiros
+//setInterval(funcao ou variavel, seguida do delay); comando de tempo
+// 
+//beginPath(); COMEÇAR UMA NOVA FORMA 
+
+FORMAS 
+//moveTo(X, Y); PONTO SUPERIOR DO TRIANGULO 
+//lineTo(X, Y); PONTO A ESQUERDA DO TRIANGULO 
+//lineTo(X, Y); PONTO A DIREITA DO TRIANGULO 
+//fillRect(1X, 1Y, 2X, 1Y) DESENHAR UM QUADRADO 
+//arc(X, Y, RAIO, ANGULO, 2 * Math.PI); DESENHAR UM CIRCULO 
+//stroke(); CONTORNO DE QUAQLER FORMA 
